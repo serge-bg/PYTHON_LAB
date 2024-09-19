98901577aba157dc09d3e4c7d33e6212b68bc5df
SergeBouopda <Sergebouopda@gmail.com>
2024-09-19 14:28:23 -0400

    add this first lab to repo. It shows how I used .format() method

---
+++ py_projects/lab1.py
@@ -0,0 +1,22 @@
+# TOPIC
+# Labs: Submit in word & github
+# Lab1: Using format () Method
+# Problem: Define three variables in your program:
+# 1.first_name: A string containing your first name.
+# 2.age: An integer representing your age.
+# 3.favorite color: A string representing your favorite color.
+# Then, using the format() method, display a sentence in the following format:
+# Hello, my name is {first name}. I am {age} years old, and my favorite color is {color}
+
+# SOLUTION
+
+# Defining three variables
+first_name = "Serge"  # Myfirst name
+age = 109  # my age in the tri-dimensional world
+favorite_color = "yellow-but"  # Replace with your favorite color
+
+# Usingclear the format() method to display the sentence
+sentence = "Hello, my name is {}. I am {} years old, and my favorite color is {}.".format(first_name, age, favorite_color)
+
+# Print the result
+print(sentence)
---
+++ py_projects/lab2.py
@@ -0,0 +1,20 @@
+# TOPIC
+# Lab2: Using f-string
+# Problem: Define two variables in your program:
+# 1. city: A string containing the name of a city.
+# 2. temperature: A floating-point number representing the current temperature in Celsius.
+# Then, using f-strings, display the sentence in the following format:
+# The current temperature in {city} is {temperature}°C
+
+# SOLUTION
+# Lab 2: Using f-strings
+
+# Defining two variables
+city = "Kigali"  # my favorite city name
+temperature = 21.5  # Today is a raining day in Kigalli
+
+# Using an f-string to display the sentence
+sentence = f"The current temperature in {city} is {temperature}°C"
+
+# Print the result
+print(sentence)
